ce4150e22fbaf338b1d8405275377fadafcf59c0
Riwma1112 <tanawattammaros@gmail.com>
2026-06-15 10:16:24 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+      <h1>Welcome to My Kingdom</h1>
+      <h1>Jungle pocket so cute</h1>
+     
+    </div>
+  );
+}
