c2cb24ae7a0317a254a9b767a4f80da294dafe16
Riwma1112 <tanawattammaros@gmail.com>
2026-08-24 09:59:43 +0700

    update

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,17 +3,17 @@ import { getSession } from "@/lib/session";
 
 export default async function Profile() {
   const user = await getSession();
-
   if (!user) {
     redirect("/login");
   }
 
   return (
-    <div className="page">
-      <h1>Profile</h1>
+    <div >
+      <h2> Profile </h2>
       <p>Name: {user.name as string}</p>
-      
       <p>Role: {user.role as string}</p>
+      <p>Email: {user.email as string}</p>     
+      <p>School: {user.school as string}</p>     
     </div>
   );
 }
